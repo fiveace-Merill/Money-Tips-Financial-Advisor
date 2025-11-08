2f800514d45863b31dad3541357f99546d557b0e
Fredrick Ogutu <freddie.ogutu@gmail.com>
2025-11-08 14:36:40 +0300

    fix: overflow on y-axis set to scroll for the Chat window

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -10,8 +10,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <SidebarWrapper />
         </div>
       </div>
-      <section className="w-full basis-11/12 p-2 flex-auto max-w-screen max-h-screen flex flex-col justify-between md:gap-6 lg:items-center lg:rounded-2xl lg:gap-3 lg:basis-9/12 lg:min-h-screen">
-        <div className="basis-11/12 w-full h-full rounded-xl bg-background-container/20 border-2 border-background-container md:mx-auto md:w-10/12 lg:flex-1 lg:pt-3 lg:w-11/12 lg:rounded-2xl">
+      <section className="w-full basis-11/12 p-2 flex-auto max-w-screen max-h-screen flex flex-col justify-between gap-3 md:gap-6 lg:items-center lg:rounded-2xl lg:gap-3 lg:basis-9/12 lg:min-h-screen">
+        <div className="basis-11/12 overflow-y-scroll w-full h-full rounded-xl bg-background-container/20 border-2 border-background-container md:mx-auto md:w-10/12 lg:flex-1 lg:pt-3 lg:w-11/12 lg:rounded-2xl">
           {children}
         </div>
         <footer className="static inset-x-0 bottom-0 w-full basis-1/12 flex justify-center lg:static lg:justify-center">
