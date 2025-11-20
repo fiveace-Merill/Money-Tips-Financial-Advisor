d3a958e5cf514b20be4afff2cac0cf405a3820f2
Fredrick Ogutu <freddie.ogutu@gmail.com>
2025-11-20 10:47:54 +0300

    clsx for conditional styling, adjusted style for MessageBubble component -> depended on type of user state 'human' or 'ai'

--- features/chat/components/MessageBubble.tsx
+++ features/chat/components/MessageBubble.tsx
@@ -1,21 +1,23 @@
 import { lexend } from "@/fonts"
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar"
-import "clsx"
+import { clsx } from "clsx"
+
 interface Props {
   user: string,
   message: string
 }
+
 export function MessageBubble({ user, message }: Props) {
   const url = user == "ai" ? "https://www.github.com/shadcn.png" : "https://github.com/evilrabbit.png"
   return (
     <div className="flex items-start gap-4">
-      <div className="h-9 w-9 mt-2 grow-0 bg-inherit border-background-container aspect-square rounded-full flex justify-center items-center">
+      <div className={clsx('h-9 w-9 mt-2 grow-0 bg-inherit border-background-container aspect-square rounded-full flex justify-center items-center', { 'order-2': user === 'human' })}>
         <Avatar >
           <AvatarImage src={url} className="rounded-full" />
           <AvatarFallback>{user == "ai" ? "AI" : "HU"}</AvatarFallback>
         </Avatar>
       </div>
-      <div className="basis-11/12 p-2 bg-surface text-on-surface shadow-sm shadow-background/40 rounded-lg">
+      <div className={clsx('basis-11/12 p-2 shadow-sm shadow-background/40 rounded-lg', { 'bg-primary text-on-primary order-1': user === 'human', 'bg-surface text-on-surface': user === 'ai' })}>
         <p className={`${lexend.className} font-light text-sm p-3 text-md text-pretty lg:text-lg lg:text-justify`}>{message}</p>
       </div>
     </div>
